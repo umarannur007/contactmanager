0d458e6f0e3e5d19127fa744b33f56300bd28b2b
Umar Annur <umarannur007@gmail.com>
2019-03-01 12:35:36 +0100

    Updates

---
+++ precache-manifest.b63579fdb3cd5004020d9ec3042b9a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "46c77ca07f16f63bedf4",
+    "url": "/contactmanager/static/js/main.46c77ca0.chunk.js"
+  },
+  {
+    "revision": "94e2ff5b84235c79e3f7",
+    "url": "/contactmanager/static/js/1.94e2ff5b.chunk.js"
+  },
+  {
+    "revision": "46c77ca07f16f63bedf4",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "94e2ff5b84235c79e3f7",
+    "url": "/contactmanager/static/css/1.8eb13f46.chunk.css"
+  },
+  {
+    "revision": "759c189d8fcd70b5626ccc0a2bda7592",
+    "url": "/contactmanager/index.html"
+  }
+];
